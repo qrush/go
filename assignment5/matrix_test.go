c2e978177ea873ea9c8a368bb3ec337665b14449
Nick Quaranto <nick@quaran.to>
2010-04-11 21:21:57 -0400

    moar simple tests

--- assignment5/matrix_test.go
+++ assignment5/matrix_test.go
@@ -7,10 +7,37 @@ import (
 func TestZeroMatrix(t *testing.T) {
 	m, _ := Zeros(3, 3)
 	if m.Rows() != 3 {
-		t.Fail()
+		t.Error("Wrong number of rows")
 	}
 
 	if m.Cols() != 3 {
-		t.Fail()
+		t.Error("Wrong number of columns")
+	}
+
+	for i := 0; i < m.Rows(); i++ {
+		for j := 0; j < m.Cols(); j++ {
+			if m.Get(i, j) != 0 {
+				t.Error("Not zeroed matrix")
+			}
+		}
+	}
+}
+
+func TestOnesMatrix(t *testing.T) {
+	m, _ := Ones(2, 5)
+	if m.Rows() != 2 {
+		t.Error("Wrong number of rows")
+	}
+
+	if m.Cols() != 5 {
+		t.Error("Wrong number of columns")
+	}
+
+	for i := 0; i < m.Rows(); i++ {
+		for j := 0; j < m.Cols(); j++ {
+			if m.Get(i, j) != 1 {
+				t.Error("Not one'd matrix")
+			}
+		}
 	}
 }
