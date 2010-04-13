b94b0b9b152e22d961b9cde239c4ea33e7492f96
John Floren <john@false.(none)>
2010-04-12 21:56:34 -0400

    Added some dox, included a Scale operation

--- assignment5/matrix.go
+++ assignment5/matrix.go
@@ -1,3 +1,4 @@
+// This package defines a Matrix interface and provides a dense matrix implementation.
 package matrix
 
 import (
@@ -5,7 +6,7 @@ import (
 	"fmt"
 )
 
-
+// Define an interface for operations a Matrix must implement.
 type Matrix interface {
 	Add(*Matrix) os.Error // Modifies target matrix
 
@@ -13,22 +14,26 @@ type Matrix interface {
 
 	Multiply(*Matrix) (*Matrix, os.Error) // Returns matrix product if successful
 
+	Scale(float) // Scale the matrix by a given value.
+
 	Get(int, int) float // Get the element at the given row & column
 
 	Set(int, int, float) // Set the specified element
 
-	Rows() int
+	Rows() int // Get the number of rows
 
-	Cols() int
+	Cols() int // Get the number of columns
 
-	Slice(int, int, int, int) (*Matrix, os.Error)
+	Slice(int, int, int, int) (*Matrix, os.Error) // Get a slice of the specified matrix
 }
 
+// Implements a dense matrix
 type DenseMatrix struct {
 	rows, cols int
 	data       []float
 }
 
+// Return a new Matrix of all zeros
 func Zeros(rows, cols int) (*Matrix, os.Error) {
 	var ret Matrix
 	ret = new(DenseMatrix)
@@ -38,6 +43,7 @@ func Zeros(rows, cols int) (*Matrix, os.Error) {
 	return &ret, nil
 }
 
+// Return a new Matrix of all ones
 func Ones(rows, cols int) (*Matrix, os.Error) {
 	var ret Matrix
 	ret = new(DenseMatrix)
@@ -50,6 +56,7 @@ func Ones(rows, cols int) (*Matrix, os.Error) {
 	return &ret, nil
 }
 
+// Return a string representing the matrix
 func (this *DenseMatrix) String() string {
 	var s string
 	s += "["
@@ -67,10 +74,13 @@ func (this *DenseMatrix) String() string {
 	return s
 }
 
+// Get the number of rows in the matrix
 func (this *DenseMatrix) Rows() int { return this.rows }
 
+// Get the number of columns in the matrix
 func (this *DenseMatrix) Cols() int { return this.cols }
 
+// Add the argument matrix to the receiver. 
 func (this *DenseMatrix) Add(m *Matrix) os.Error {
 	if this.Rows() != m.Rows() || this.Cols() != m.Cols() {
 		return os.NewError("Matrix dimensions do not match")
@@ -83,6 +93,7 @@ func (this *DenseMatrix) Add(m *Matrix) os.Error {
 	return nil
 }
 
+// Return a new Matrix containing the sum of the receiver and the argument.
 func (this *DenseMatrix) Plus(m *Matrix) (*Matrix, os.Error) {
 	if this.Rows() != m.Rows() || this.Cols() != m.Cols() {
 		return nil, os.NewError("Matrix dimensions do not match")
@@ -98,6 +109,7 @@ func (this *DenseMatrix) Plus(m *Matrix) (*Matrix, os.Error) {
 	return ret, nil
 }
 
+// Get the specified element.
 func (this *DenseMatrix) Get(row, col int) float {
 	if (row < this.Rows()) && (col < this.Cols()) && (row >= 0) && (col >= 0) {
 		return this.data[(row*this.Cols())+col]
@@ -106,20 +118,16 @@ func (this *DenseMatrix) Get(row, col int) float {
 	return 0 // this is wrong
 }
 
+// Set the specified element.
 func (this *DenseMatrix) Set(row, col int, val float) {
 	this.data[(row*this.Cols())+col] = val
 }
 
+// Return the matrix consisting of the matrix product of the argument and the receiver.
 func (this *DenseMatrix) Multiply(m *Matrix) (*Matrix, os.Error) {
 	if this.Cols() != m.Rows() {
 		return nil, os.NewError("Invalid matrix dimensions for Multiply")
 	}
-/*
-	ret := new(DenseMatrix)
-	ret.data = make([]float, this.rows*m.cols)
-	ret.rows = this.rows
-	ret.cols = m.cols
-*/
 	ret,_ := Zeros(this.Rows(), this.Cols())
 
 	for i := 0; i < this.Rows(); i++ {
@@ -134,6 +142,19 @@ func (this *DenseMatrix) Multiply(m *Matrix) (*Matrix, os.Error) {
 	return ret, nil
 }
 
+// Scale the receiver by the given value.
+func (this *DenseMatrix) Scale(s float) {
+	for i := 0; i < this.Rows(); i++ {
+		for j := 0; j < this.Cols(); j++ {
+			t := this.Get(i, j)
+			this.Set(i, j, t*s)
+		}
+	}
+}
+
+// Return a new Matrix containing the specified range of the receiver. The starting rows and columns
+// are inclusive, while the ending rows and columns are non-inclusive, as in the slicing operator. Thus,
+// m.Slice(1, m.Rows(), 0, m.Cols()) to get rid of the first row.
 func (this *DenseMatrix) Slice(rstart, rend, cstart, cend int) (*Matrix, os.Error) {
 	if rstart >= rend || cstart >= cend {
 		return nil, os.NewError("Invalid start/end specification")
