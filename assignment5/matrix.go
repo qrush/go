dc8ec313a4cf641a9bc2c26ddaf772c298c8b1af
Nick Quaranto <nick@quaran.to>
2010-04-14 10:59:13 -0400

    Refactoring LIKE A BOSS

--- assignment5/matrix.go
+++ assignment5/matrix.go
@@ -6,32 +6,38 @@ import (
 	"fmt"
 )
 
-// Define an interface for operations a Matrix must implement.
-type Matrix interface {
-	Add(*Matrix) os.Error // Modifies target matrix
+type (
+	// Define an interface for operations a Matrix must implement.
+	Matrix interface {
+		Add(*Matrix) os.Error // Modifies target matrix
 
-	Plus(*Matrix) (*Matrix, os.Error) // Does not modify target
+		Plus(*Matrix) (*Matrix, os.Error) // Does not modify target
 
-	Multiply(*Matrix) (*Matrix, os.Error) // Returns matrix product if successful
+		Multiply(*Matrix) (*Matrix, os.Error) // Returns matrix product if successful
 
-	Scale(float) // Scale the matrix by a given value.
+		Scale(float) // Scale the matrix by a given value.
 
-	Get(int, int) float // Get the element at the given row & column
+		Get(int, int) float // Get the element at the given row & column
 
-	Set(int, int, float) // Set the specified element
+		Set(int, int, float) // Set the specified element
 
-	Rows() int // Get the number of rows
+		Rows() int // Get the number of rows
 
-	Cols() int // Get the number of columns
+		Cols() int // Get the number of columns
 
-	Slice(int, int, int, int) (*Matrix, os.Error) // Get a slice of the specified matrix
-}
+		Slice(int, int, int, int) (*Matrix, os.Error) // Get a slice of the specified matrix
 
-// Implements a dense matrix
-type DenseMatrix struct {
-	rows, cols int
-	data       []float
-}
+		ForEach(Iterator) // Do an action on each cell of the matrix
+	}
+
+	// Implements a dense matrix
+	DenseMatrix struct {
+		rows, cols int
+		data       []float
+	}
+
+	Iterator func(int, int)
+)
 
 // Return a new Matrix of all zeros
 func Zeros(rows, cols int) (*Matrix, os.Error) {
@@ -45,21 +51,14 @@ func Zeros(rows, cols int) (*Matrix, os.Error) {
 
 // Return a new Matrix of all ones
 func Ones(rows, cols int) (*Matrix, os.Error) {
-	var ret Matrix
-	ret = new(DenseMatrix)
-	ret.(*DenseMatrix).data = make([]float, rows*cols)
-	for i := range ret.(*DenseMatrix).data {
-		ret.(*DenseMatrix).data[i] = 1
-	}
-	ret.(*DenseMatrix).rows = rows
-	ret.(*DenseMatrix).cols = cols
-	return &ret, nil
+	ret, _ := Zeros(rows, cols)
+	ret.ForEach(func(i, j int) { ret.Set(i, j, 1) })
+	return ret, nil
 }
 
 // Return a string representing the matrix
 func (this *DenseMatrix) String() string {
-	var s string
-	s += "["
+	s := "["
 	for i := 0; i < this.Rows(); i++ {
 		s += "	["
 		for j := 0; j < this.Cols(); j++ {
@@ -74,6 +73,16 @@ func (this *DenseMatrix) String() string {
 	return s
 }
 
+// Run the given function on every cell of the matrix
+func (this *DenseMatrix) ForEach(iter Iterator) {
+	for i := 0; i < this.Rows(); i++ {
+		for j := 0; j < this.Cols(); j++ {
+			iter(i, j)
+		}
+	}
+	return
+}
+
 // Get the number of rows in the matrix
 func (this *DenseMatrix) Rows() int { return this.rows }
 
@@ -85,11 +94,9 @@ func (this *DenseMatrix) Add(m *Matrix) os.Error {
 	if this.Rows() != m.Rows() || this.Cols() != m.Cols() {
 		return os.NewError("Matrix dimensions do not match")
 	}
-	for i := 0; i < this.Rows(); i++ {
-		for j := 0; j < this.Cols(); j++ {
-			this.Set(i, j, this.Get(i, j)+m.Get(i, j))
-		}
-	}
+
+	this.ForEach(func(i, j int) { this.Set(i, j, this.Get(i, j)+m.Get(i, j)) })
+
 	return nil
 }
 
@@ -98,20 +105,15 @@ func (this *DenseMatrix) Plus(m *Matrix) (*Matrix, os.Error) {
 	if this.Rows() != m.Rows() || this.Cols() != m.Cols() {
 		return nil, os.NewError("Matrix dimensions do not match")
 	}
-	//ret.data = make([]float, this.rows*this.cols)
 	var ret *Matrix
 	ret, _ = Zeros(this.Rows(), this.Cols())
-	for i := 0; i < this.Rows(); i++ {
-		for j := 0; j < this.Cols(); j++ {
-			ret.Set(i, j, this.Get(i, j)+m.Get(i, j))
-		}
-	}
+	this.ForEach(func(i, j int) { ret.Set(i, j, this.Get(i, j)+m.Get(i, j)) })
 	return ret, nil
 }
 
 // Get the specified element.
 func (this *DenseMatrix) Get(row, col int) float {
-	if (row < this.Rows()) && (col < this.Cols()) && (row >= 0) && (col >= 0) {
+	if row < this.Rows() && col < this.Cols() && row >= 0 && col >= 0 {
 		return this.data[(row*this.Cols())+col]
 	}
 	fmt.Println("invalid row/col index")
@@ -130,26 +132,22 @@ func (this *DenseMatrix) Multiply(m *Matrix) (*Matrix, os.Error) {
 	}
 	ret, _ := Zeros(this.Rows(), m.Cols())
 
-	for i := 0; i < this.Rows(); i++ {
-		for j := 0; j < m.Cols(); j++ {
-			var sum float
-			for k := 0; k < this.Cols(); k++ {
-				sum += this.Get(i, k) * m.Get(k, j)
-			}
-			ret.Set(i, j, sum)
+	ret.ForEach(func(i, j int) {
+		var sum float
+		for k := 0; k < this.Cols(); k++ {
+			sum += this.Get(i, k) * m.Get(k, j)
 		}
-	}
+		ret.Set(i, j, sum)
+	})
 	return ret, nil
 }
 
 // Scale the receiver by the given value.
 func (this *DenseMatrix) Scale(s float) {
-	for i := 0; i < this.Rows(); i++ {
-		for j := 0; j < this.Cols(); j++ {
-			t := this.Get(i, j)
-			this.Set(i, j, t*s)
-		}
-	}
+	this.ForEach(func(i, j int) {
+		t := this.Get(i, j)
+		this.Set(i, j, t*s)
+	})
 }
 
 // Return a new Matrix containing the specified range of the receiver. The starting rows and columns
@@ -160,10 +158,6 @@ func (this *DenseMatrix) Slice(rstart, rend, cstart, cend int) (*Matrix, os.Erro
 		return nil, os.NewError("Invalid start/end specification")
 	}
 	ret, _ := Zeros(rend-rstart, cend-cstart)
-	for i := 0; i < rend-rstart; i++ {
-		for j := 0; j < cend-cstart; j++ {
-			ret.Set(i, j, this.Get(i+rstart, j+cstart))
-		}
-	}
+	ret.ForEach(func(i, j int) { ret.Set(i, j, this.Get(i+rstart, j+cstart)) })
 	return ret, nil
 }
