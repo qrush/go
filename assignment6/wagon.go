52605c9b55a5976dda136448c94694207ce1c0ff
John Floren <slawmaster@gmail.com>
2010-04-17 19:41:07 -0400

    Added ANSI functions

--- assignment6/wagon.go
+++ assignment6/wagon.go
@@ -1,5 +1,21 @@
 package main
 
 import "fmt"
+import "time"
 
-func main() { fmt.Println("WAGON TRAIN") }
+func redraw() {
+}
+
+func clearScreen() {
+	fmt.Printf("\033[2J\n")
+}
+
+func drawAt(x, y int, s string) {
+	fmt.Printf("\033[%d;%dH%s\n", x, y, s)
+}
+
+func main() { 
+	clearScreen()
+	drawAt(2,3,"A")
+	time.Sleep(30000000000)
+}
