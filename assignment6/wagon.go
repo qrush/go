6e98c1315ec41f4ffb65cc9277b82a3b5ec7dc8f
Nick Quaranto <nick@quaran.to>
2010-04-17 19:52:49 -0400

    container/list is pretty cool

--- assignment6/wagon.go
+++ assignment6/wagon.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
-import "time"
+import "container/list"
+
+type Wagon struct {
+	x, y    int
+	display string
+}
 
 func redraw() {
 }
@@ -17,5 +22,29 @@ func drawAt(x, y int, s string) {
 func main() { 
 	clearScreen()
 	drawAt(2,3,"A")
-	time.Sleep(30000000000)
+
+	fmt.Println("WAGON TRAIN")
+
+	l := list.New()
+	w1 := new(Wagon)
+	w2 := new(Wagon)
+	w3 := new(Wagon)
+
+	w1.x = 3
+	w1.y = 3
+	w1.display = "a"
+	w2.x = 4
+	w2.y = 4
+	w2.display = "b"
+	w3.x = 5
+	w3.y = 5
+	w3.display = "c"
+
+	l.PushBack(w1)
+	l.PushBack(w2)
+	l.PushBack(w3)
+
+	for w := range l.Iter() {
+		fmt.Println(w.(*Wagon).display)
+	}
 }
