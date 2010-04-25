9d998d971a3584a5e6552a4a52958830611d87e3
John Floren <slawmaster@gmail.com>
2010-04-25 16:49:50 -0400

    Gots us some localview

--- assignment7/games/games.go
+++ assignment7/games/games.go
@@ -1,6 +1,9 @@
 package games
 
 import "os"
+import "io"
+import "bufio"
+import "fmt"
 
 // outcome of a game; positive.
 type Outcome int
@@ -31,4 +34,49 @@ type View interface {
 
 	// view's event loop, could return a read error.
 	Loop() os.Error
+}
+
+type LocalView struct {
+	gotMove chan bool
+	name string
+	myMove, otherMove string
+	in  *bufio.Reader
+	out *bufio.Writer
+}
+
+func NewLocalView(name string, reader io.Reader, writer io.Writer) View {
+	var view View
+	view      = new(LocalView)
+	view.(*LocalView).name = name
+	view.(*LocalView).in   = bufio.NewReader(reader)
+	view.(*LocalView).out  = bufio.NewWriter(writer)
+	return view
+} 
+
+func (this *LocalView) Enable() {
+	this.Loop()
+	this.gotMove <- true
+}
+
+func (this *LocalView) Get() interface{} {
+	<-this.gotMove
+	foo := this.myMove
+	return foo
+}
+
+func (this *LocalView) Set(move interface{}) {
+	this.otherMove = move.(string)
+}
+
+func (this *LocalView) Display() {
+	fmt.Printf("%s's opponent's move: %s\n", this.name, this.otherMove)	
+}
+
+func (this *LocalView) Done(youWin Outcome) {
+}
+
+func (this *LocalView) Loop() os.Error {
+	tmp, ok := this.in.ReadBytes('\n')
+	this.myMove = string(tmp)
+	return ok
 }
--- assignment7/rps/rps.go
+++ assignment7/rps/rps.go
@@ -1,5 +1,16 @@
 package rps
 
-import "fmt"
+import "games"
+import "os"
 
-func Test() { fmt.Println("IN RPS") }
+var (
+	player1 *games.View
+	player2 *games.View
+)
+
+func Play(path string) {
+	player1 = games.NewLocalView("A", os.Stdin, os.Stdout)
+
+	file, _ := os.Open(path, os.O_RDWR, 0)
+	player2 = games.NewLocalView("B", file, file)
+} 
