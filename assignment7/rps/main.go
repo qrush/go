a3d6eb91d62da25487bd414178802d6f7c1bb0c3
John Floren <slawmaster@gmail.com>
2010-04-26 18:32:19 -0400

    RPS now works with zero arguments

--- assignment7/rps/main.go
+++ assignment7/rps/main.go
@@ -4,9 +4,16 @@ import (
 	"games"
 	"rps"
 	"os"
+	"fmt"
 )
 
 func main() {
-	file, _ := os.Open(os.Args[1], os.O_RDWR, 0)
-	games.Play(os.Stdin, os.Stdout, file, file, rps.NewReferee())
+	if len(os.Args) == 2 {
+		file, _ := os.Open(os.Args[1], os.O_RDWR, 0)
+		games.Play(os.Stdin, os.Stdout, file, file, rps.NewReferee())
+	} else if len(os.Args) == 1 {
+		games.Play(os.Stdin, os.Stdout, os.Stdin, os.Stdout, rps.NewReferee())
+	} else {
+		fmt.Printf("Invalid arguments. Usage: %s [terminal]", os.Args[0])
+	}
 }
